5b89864ffe6d83e0272774b943a63cc19488be59
fernandoduarteri <fduarte@gs1ni.org>
2021-08-03 09:38:18 -0600

    Cambios para poder pagar mas de una cuota

--- src/dao/PrestamoDAO.java
+++ src/dao/PrestamoDAO.java
@@ -102,4 +102,56 @@ public class PrestamoDAO extends JPAEntity {
 		
 	}
 	
+	public void getPrestamoUnicoCliente(ObjectReturn objReturn) {
+		int idPrestamo= (int) objReturn.getData();
+		
+		
+		String query = "select cc.venc_permitido, cc.ld_permitida, cc.id as id_cliente, p.id, p.observaciones, CEIL((p.monto * p.porcentaje_interes / 100) + p.monto + IFNULL(p.porcentaje_mora,0)) as monto, p.cuotas, CEIL((monto_cuota - IFNULL(sum(pp.monto_pagado),0))) as adeudado,"
+				+ "               cc.cedula, c.id as id_cuota, c.numero, cc.nombre, cc.apellidos, IFNULL(c.monto_cuota - sum(pp.monto_pagado), c.monto_cuota) as monto_cuota, DATE_FORMAT(p.fecha_vencimiento, '%Y-%d-%m') as fecha_vencimiento, monto_total from prestamo p"
+				+ "             inner join cuota c"
+				+ "                on c.prestamo = p.id"
+				+ "                left join pago pp"
+				+ "               on c.id = pp.cuota"
+				+ "                inner join cliente cc"
+				+ "               on cc.id = p.cliente"
+				+ "                where p.id="+idPrestamo+ " and p.pagado = 0 and c.pagada = 0";
+		try {
+			
+			
+			HashMap<String, Object> mapconvert = new HashMap<String,Object>();
+			
+			List<Object[]> getList  = super.findAllNative(query);
+			for(Object[] q1 : getList){
+				 mapconvert.put("venc_permitido",q1[0]);
+				 mapconvert.put("ld_permitida",q1[1]);
+				 mapconvert.put("id_cliente",q1[2]);
+				 mapconvert.put("id",q1[3]);
+				 mapconvert.put("observaciones",q1[4]);
+				 mapconvert.put("monto",q1[5]);
+				 mapconvert.put("cuotas",q1[6]);
+				 mapconvert.put("adeudado",q1[7]);
+				 mapconvert.put("cedula",q1[8]);
+				 mapconvert.put("id_cuota",q1[9]);
+				 mapconvert.put("numero",q1[10]);
+				 mapconvert.put("nombre",q1[11]);
+				 mapconvert.put("apellidos",q1[12]);
+				 mapconvert.put("monto_cuota",q1[13]);
+				 mapconvert.put("fecha_vencimiento",q1[14]);
+				 mapconvert.put("monto_total",q1[15]);
+				
+			     }
+			objReturn.setData(mapconvert);
+			objReturn.setMensaje("Exito");
+			objReturn.setExito(Constantes.FLAG_EXITO_EXITO);
+			objReturn.setTotal(mapconvert.size());
+		}
+		catch(Exception e) {
+				objReturn.setData("");
+				objReturn.setMensaje(e.getMessage());
+				objReturn.setExito(Constantes.FLAG_EXITO_FALLA);
+				objReturn.setTotal(0);
+			}
+		
+	}
+	
 }
