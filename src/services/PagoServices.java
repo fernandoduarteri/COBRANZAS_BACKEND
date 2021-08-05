702a1236016e926f2ec494bbc408ea044b395e99
fernandoduarteri <fduarte@gs1ni.org>
2021-08-04 19:40:45 -0600

    Agregando la validacion de que el monto pagado no puede ser mayor que el monto total adeudado del prestamos

--- src/services/PagoServices.java
+++ src/services/PagoServices.java
@@ -18,78 +18,95 @@ PagoDAO objPagoDao = new PagoDAO (Pago.class);
 		Integer veces=0;
 		String retorno= "";
 		double saldo = 0;
+		double adeudoTotalPrestamo=0;
 		int idPrestamo = Integer.valueOf( pago.getCuotaBean().getPrestamoBean().getId());
-		if(pago.getMontoPagado()>pago.getCuotaBean().getMontoCuota()) {
-			// Aca hay que tomar en consideracion que si la cuota que viene del frontEnd es un saldo que no corresponde a la cuota normal del prestamo
-			// lo cual nos daria unas iteraciones falsas, para pagar ese saldo se debe de sumar esa iteracion al contador
-			// Dado que si hay un saldo, al consultar esa cuot en especifico no nos dice la consulta si la cuota normal es mayor, por lo tanto se tiene que buscar el prestamo y verificar
-			axObjReturn.setData(idPrestamo);
-			getPrestamo(axObjReturn);
-			HashMap<String,Object> resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
-			double montoTotal = (double) resultPrestamo.get("monto_total");
-			Integer cuotas = (Integer) resultPrestamo.get("cuotas");
-			double cuota = montoTotal / cuotas;
-			if((montoTotal / cuotas ) == pago.getCuotaBean().getMontoCuota()) {
-				//El montoCuota que viene del frontEnd no es un saldo, por tanto se puede calcular el numero de veces como sigue
-				veces = (int) Math.ceil( pago.getMontoPagado() / pago.getCuotaBean().getMontoCuota());
+		
+		
+		try {
+			if(pago.getMontoPagado()>pago.getCuotaBean().getMontoCuota()) {
+				// Aca hay que tomar en consideracion que si la cuota que viene del frontEnd es un saldo que no corresponde a la cuota normal del prestamo
+				// lo cual nos daria unas iteraciones falsas, para pagar ese saldo se debe de sumar esa iteracion al contador
+				// Dado que si hay un saldo, al consultar esa cuot en especifico no nos dice la consulta si la cuota normal es mayor, por lo tanto se tiene que buscar el prestamo y verificar
+				axObjReturn.setData(idPrestamo);
+				getPrestamo(axObjReturn);
+				HashMap<String,Object> resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
+				double montoTotal = (double) resultPrestamo.get("monto_total");
+				Integer cuotas = (Integer) resultPrestamo.get("cuotas");
+				double cuota = montoTotal / cuotas;
+				if((montoTotal / cuotas ) == pago.getCuotaBean().getMontoCuota()) {
+					//El montoCuota que viene del frontEnd no es un saldo, por tanto se puede calcular el numero de veces como sigue
+					veces = (int) Math.ceil( pago.getMontoPagado() / pago.getCuotaBean().getMontoCuota());
+					adeudoTotalPrestamo = (1+ ((int)resultPrestamo.get("cuotas") - (int)resultPrestamo.get("numero")))*pago.getCuotaBean().getMontoCuota();
+					
+				}else {
+					//El montoCuota que viene del frontEnd es un saldo y el numero de veces se tiene que calcular como sigue
+					veces = (int) Math.ceil(1 + (pago.getMontoPagado()- pago.getCuotaBean().getMontoCuota())/cuota);
+					adeudoTotalPrestamo = ((((int)resultPrestamo.get("cuotas") - (int)resultPrestamo.get("numero")))*cuota) + pago.getCuotaBean().getMontoCuota();
+					
+				}
+				if(pago.getMontoPagado()>adeudoTotalPrestamo) {
+					throw new Exception("El monto pagado no puede ser mahyor que el adeudo total del prestamo " + adeudoTotalPrestamo);
+				}
 				
+				for (int i = 1; i <= veces; i++) {
+					axObjReturn = new ObjectReturn();
+					Pago pagoMultiple = new Pago();
+					pagoMultiple.setUsuario(pago.getUsuario());
+					Cuota cuotaMultiple = new Cuota();
+					if(i == 1) { //El idcuota es el que viene del frontEnd
+						cuotaMultiple.setId(pago.getCuotaBean().getId());
+						pagoMultiple.setCuotaBean(cuotaMultiple); // se envia el idCuota que viene del frontend
+						pagoMultiple.setMontoPagado(pago.getCuotaBean().getMontoCuota()); // se paga el valor de la cuota que viene del front end, sea cuota norma o saldo
+						axObjReturn.setData(idPrestamo);
+						getPrestamo(axObjReturn);
+						resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
+						axObjReturn.setData(pagoMultiple);
+						objPagoDao.hacerPago(axObjReturn);
+						saldo = pago.getMontoPagado() - pago.getCuotaBean().getMontoCuota();
+						retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+pago.getCuotaBean().getMontoCuota() +"\n";
+						continue;
+					}
+					
+					if(i== veces) {
+						
+						axObjReturn.setData(idPrestamo);
+						getPrestamo(axObjReturn);
+						resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
+						cuotaMultiple.setId(String.valueOf( resultPrestamo.get("id_cuota"))); //Se obtiene la sigueinte cuoat a pagar
+						pagoMultiple.setCuotaBean(cuotaMultiple);
+						double montoPagado = saldo;
+						pagoMultiple.setMontoPagado(montoPagado); // se paga el valor del remanente del montoPagado
+						axObjReturn.setData(pagoMultiple);
+						objPagoDao.hacerPago(axObjReturn);
+						retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+montoPagado +"\n"+ axObjReturn.getData() + "\n";
+						objReturn.setData(retorno);
+						objReturn.setMensaje("Exito");
+						objReturn.setExito(Constantes.FLAG_EXITO_EXITO);
+						objReturn.setTotal(1);
+					}else {
+						axObjReturn.setData(idPrestamo);
+						getPrestamo(axObjReturn);
+						resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
+						cuotaMultiple.setId(String.valueOf(resultPrestamo.get("id_cuota"))); //Se obtiene la sigueinte cuoat a pagar
+						pagoMultiple.setCuotaBean(cuotaMultiple);
+						pagoMultiple.setMontoPagado(cuota); // se paga el valor de la cuota real
+						axObjReturn.setData(pagoMultiple);
+						objPagoDao.hacerPago(axObjReturn);
+						saldo = saldo - cuota;
+						retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+cuota +"\n";
+					}
+				}
 			}else {
-				//El montoCuota que viene del frontEnd es un saldo y el numero de veces se tiene que calcular como sigue
-				veces = (int) Math.ceil(1 + (pago.getMontoPagado()- pago.getCuotaBean().getMontoCuota())/cuota);
-				
+				objPagoDao.hacerPago(objReturn);	
 			}
 			
-			for (int i = 1; i <= veces; i++) {
-				axObjReturn = new ObjectReturn();
-				Pago pagoMultiple = new Pago();
-				pagoMultiple.setUsuario(pago.getUsuario());
-				Cuota cuotaMultiple = new Cuota();
-				if(i == 1) { //El idcuota es el que viene del frontEnd
-					cuotaMultiple.setId(pago.getCuotaBean().getId());
-					pagoMultiple.setCuotaBean(cuotaMultiple); // se envia el idCuota que viene del frontend
-					pagoMultiple.setMontoPagado(pago.getCuotaBean().getMontoCuota()); // se paga el valor de la cuota que viene del front end, sea cuota norma o saldo
-					axObjReturn.setData(idPrestamo);
-					getPrestamo(axObjReturn);
-					resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
-					axObjReturn.setData(pagoMultiple);
-					objPagoDao.hacerPago(axObjReturn);
-					saldo = pago.getMontoPagado() - pago.getCuotaBean().getMontoCuota();
-					retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+pago.getCuotaBean().getMontoCuota() +"\n";
-					continue;
-				}
-				
-				if(i== veces) {
-					
-					axObjReturn.setData(idPrestamo);
-					getPrestamo(axObjReturn);
-					resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
-					cuotaMultiple.setId(String.valueOf( resultPrestamo.get("id_cuota"))); //Se obtiene la sigueinte cuoat a pagar
-					pagoMultiple.setCuotaBean(cuotaMultiple);
-					double montoPagado = saldo;
-					pagoMultiple.setMontoPagado(montoPagado); // se paga el valor del remanente del montoPagado
-					axObjReturn.setData(pagoMultiple);
-					objPagoDao.hacerPago(axObjReturn);
-					retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+montoPagado +"\n"+ axObjReturn.getData() + "\n";
-					objReturn.setData(retorno);
-					objReturn.setMensaje("Exito");
-					objReturn.setExito(Constantes.FLAG_EXITO_EXITO);
-					objReturn.setTotal(1);
-				}else {
-					axObjReturn.setData(idPrestamo);
-					getPrestamo(axObjReturn);
-					resultPrestamo = (HashMap<String, Object>) axObjReturn.getData();
-					cuotaMultiple.setId(String.valueOf(resultPrestamo.get("id_cuota"))); //Se obtiene la sigueinte cuoat a pagar
-					pagoMultiple.setCuotaBean(cuotaMultiple);
-					pagoMultiple.setMontoPagado(cuota); // se paga el valor de la cuota real
-					axObjReturn.setData(pagoMultiple);
-					objPagoDao.hacerPago(axObjReturn);
-					saldo = saldo - cuota;
-					retorno = retorno + "Cuota "+ resultPrestamo.get("numero") +"/" + resultPrestamo.get("cuotas") + " $"+cuota +"\n";
-				}
-			}
-		}else {
-			objPagoDao.hacerPago(objReturn);	
+		}catch(Exception e) {
+			objReturn.setData("Error al querer grabar los pagos");
+			objReturn.setMensaje(e.getMessage());
+			objReturn.setExito(Constantes.FLAG_EXITO_FALLA);
+			objReturn.setTotal(0);
 		}
+		
 	}
 	
 	public void getPrestamo(ObjectReturn objReturn) {
